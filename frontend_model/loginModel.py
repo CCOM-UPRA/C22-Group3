08f2c41e92c1d4703f0e0664d6a83ae3ffb463af
Emanuel Martinez <126026176+EmanuelCCOM@users.noreply.github.com>
2023-04-15 00:42:06 -0400

    fixed like half of the page.
    algunas cosas aun no se muestran pero logre que el boton del check out apareciera WUJUUUU (casi muero en el proceso).

--- frontend_model/loginModel.py
+++ frontend_model/loginModel.py
@@ -7,16 +7,15 @@ def loginmodel(email, password):
     # Receive email and password to check in the "database"
 
     user = []
-    conn = pymysql.connect(host='sql9.freemysqlhosting.net', db='sql9602731',
-                           user='sql9602731', password='zChRVJs2Nf', port=3306)
+    conn = pymysql.connect(host='sql9.freemysqlhosting.net', db='sql9607918',
+                           user='sql9607918', password='GFQC75Bg2g', port=3306)
     cur = conn.cursor()
-    cur.execute("SELECT * from customer WHERE c_email = %s", email)
+    cur.execute("SELECT * from customers WHERE c_email = %s", email)
     userFound = cur.fetchall()
     for users in userFound:
-        user.append({"id": users[0], "name": users[1], "last_name": users[2], "address_line1": users[3],
-                    "address_line2": users[4], "city": users[5], "state": users[6], "zipcode": users[7],
-                    "email": users[8], "password": users[9], "phone_number": users[10], "card_name": users[11],
-                    "card_type": users[12], "card_number": users[13], "exp_date": users[14], "status": users[15]})
+        user.append({"id": users[0], "name": users[1], "last_name": users[2], "city": users[7],
+                     "state": users[8], "zipcode": users[9], "email": users[3], "password": users[4],
+                     "phone_number": users[5], "status": users[10], "street": users[6]})
 
     # Save user info in list
 
@@ -24,8 +23,8 @@ def loginmodel(email, password):
     # sha256_crypt.verify(password_unhashed, password_hashed) = this is what is used to compare an unhashed and hashed password
 
     for u in user:
-        print("Hashed password from user: ", u['password'])
-        if email == u['email'] and sha256_crypt.verify(password, u['password']) is True:
+        #print("Hashed password from user: ", u['password'])
+        if email == u['email'] and password == u['password']:
             session['customer'] = u['id']
             # Create the session['customer'] saving the customer ID if user is found
             return "true"
