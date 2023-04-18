b474edb8dc98e1ac8dae01146e2e84526ab1e2d1
Emanuel Martinez <126026176+EmanuelCCOM@users.noreply.github.com>
2023-04-17 23:03:38 -0400

    cambiar la contrasena ahora funciona correctamente
    no funcionaba pero ahora si con este commit.

--- frontend_controller/profileController.py
+++ frontend_controller/profileController.py
@@ -1,5 +1,5 @@
 from passlib.handlers.sha2_crypt import sha256_crypt
-
+from flask import session
 from frontend_model.profileModel import *
 
 
@@ -24,32 +24,16 @@ def editprofilecontroller(fname, lname, email):
 
 
 def changePass(npassw, email):
-    user = []
     # Connect to MySQL database server using credentials provided
     conn = pymysql.connect(host='sql9.freemysqlhosting.net', db='sql9607918',
                            user='sql9607918', password='GFQC75Bg2g', port=3306)
-    
-    cur = conn.cursor()
-    # Select the specific customer whose password you want to hash
-    cur.execute("SELECT * from customer WHERE c_email = %s", (email,))
-    userFound = cur.fetchall()
-
-    # This part isn't necessary here but is shown so student can visualize the DB structure
-    for users in userFound:
-        user.append({"id": users[0], "name": users[1], "last_name": users[2], "city": users[7],
-                     "state": users[8], "zipcode": users[9], "email": users[3], "password": users[4],
-                     "phone_number": users[5], "status": users[10], "street": users[6]})
-        # Save the user's password in 'passw'
-        users[4] = npassw
-
-    # Encrypt the password using the sha256_crypt function
-    hash = sha256_crypt.encrypt(npassw)
-    print("Hashed password: ", hash)
 
     try:
-        # Once encrypted, save this new hashed password to DB
-        cur.execute("UPDATE customer SET c_password = %s WHERE c_email = %s", (hash, email))
+        cur = conn.cursor()
+        cur.execute("UPDATE customers SET c_password = %s WHERE c_email = %s AND customer_id = %s", (npassw, email, session['customer']))
         conn.commit()
+        conn.close()
+        cur.close()
         return 1
 
     except pymysql.Error as error:
