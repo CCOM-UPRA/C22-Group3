9b23ecf59a7bd344d24d9db70c925e478115cf0c
Emanuel Martinez <126026176+EmanuelCCOM@users.noreply.github.com>
2023-08-07 22:22:44 -0400

    las ordenes se presentan correctamente en frontend

--- frontend_model/ordersModel.py
+++ frontend_model/ordersModel.py
@@ -14,20 +14,19 @@ def getOrderModel():
     conn = pymysql.connect(host='sql9.freemysqlhosting.net', db='sql9607918',
                            user='sql9607918', password='GFQC75Bg2g', port=3306)
     cur = conn.cursor()
-    cur.execute("SELECT DISTINCT tracking_number, price_total, day, order_id FROM orders NATURAL JOIN cont",)
+    cur.execute("SELECT DISTINCT orders.tracking_number, orders.price_total, cont.day, orders.order_id FROM orders JOIN cont ON orders.order_id = cont.order_id WHERE orders.customer_id = %s;",(session['customer']))
     results = cur.fetchall()
+    print(results)
     
     for res in results:
         products = []
         amountproducts = 0
-        info = [session['customer'], res[3]]
-        inf = tuple(info)
-        cur.execute("SELECT s_name, image_link, s_brand, price, quantity FROM orders NATURAL JOIN cont NATURAL JOIN stickers WHERE customer_id = %s AND order_id = %s GROUP BY s_name", inf)
+        cur.execute("SELECT s_name, image_link, s_brand, cont.price, cont.quantity FROM cont JOIN stickers ON cont.sticker_id = stickers.sticker_id WHERE cont.order_id = %s;",(res[3]))
         results2 = cur.fetchall()
-        
         for res2 in results2:
-            products.append({"s_name": res2[0], "img": res2[1], "brand": res2[2], "price": res2[3], "quantity": res2[4],})
-            amountproducts = amountproducts + res2[4]
+            products.append({"s_name": res2[0], "img": res2[1], "brand": res2[2], "price": res2[3], "quantity": res2[4]})
+            amountproducts += res2[4]
+        
         orderlist.append({"tracking": res[0], "total": res[1], "date": res[2], "o_id": res[3], "products": products, "amount": amountproducts})
     
     cur.close()
